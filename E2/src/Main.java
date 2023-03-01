0ec6f9e6c8184da4ecb3a487da22d00a8317dcbc
Bition <656573824@qq.com>
2023-03-01 16:42:03 +0800

    a work in 22.03.01

--- E2/src/Main.java
+++ E2/src/Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello 456!");
-        System.out.println("Hello 123!");
+        Scanner user = new Scanner(System.in);
+        System.out.println("please enter 2 digits");
+        int number = user.nextInt();
+        Random r = new Random();
+        int reward = r.nextInt(100);
+        int count = 0;
+        if (number == reward){
+            System.out.println("Reward is $10000");
+        }else {
+            if (number%10 == reward%10){
+                count++;
+            }
+            if (number/10 == reward%10){
+                count++;
+            }
+            if (number%10 == reward/10){
+                count++;
+            }
+            if (number/10 == reward/10){
+                count++;
+            }
+            switch (count){
+                case 0 -> System.out.println("Reward is 0");
+                case 1 -> System.out.println("Reward is $1000");
+                case 2 -> System.out.println("Reward is $3000");
+            }
+        }
     }
 }
